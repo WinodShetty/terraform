80f630148ad39a5ab6c082e464bbd2fd7de1f64a
vinod <mechvnod@gmail.com>
2025-04-02 21:11:44 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
-# resource "aws_instance" "this" {
-#   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
-#   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-#   instance_type          = "t3.micro"
-#   tags = {
-#     Name    = "terraform-demo"
-#     Purpose = "terraform-practice"
-#   }
-# }
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
+  tags = {
+    Name    = "terraform-demo"
+    Purpose = "terraform-practice"
+  }
+}
 
 
 resource "aws_security_group" "allow_tls" {
