dcf6a105d03f777b0711722f506d6294e144ae6d
vinod <mechvnod@gmail.com>
2025-04-13 20:35:48 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,32 @@
-resource "aws_instance" "this" {
-  ami                     = "ami-0dcc1e21636832c5d"
-  instance_type           = "m5.large"
-  host_resource_group_arn = "arn:aws:resource-groups:us-west-2:123456789012:group/win-testhost"
-  tenancy                 = "host"
+resource "aws_instance" "terraform_sample" {
+  ami = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.win.id]
+  instance_type = "t3.micro"
+  tags = {
+    name = "terraform-demo"
+    purpose = "terraform-practice"
+  }
+}
+
+resource "aws_security_group" "win" {
+  name = "win"
+  description = "allow TLS inbound traffic and all out bound traffic"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress  {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "win_terraform"
+  }
 }
