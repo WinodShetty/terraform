388082e5271f32d8fc71ef36d53a574fe7d9efdb
vinod <mechvnod@gmail.com>
2025-06-24 20:30:40 +0530

    import

--- import/provider.tf
+++ import/provider.tf
@@ -6,12 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "82s-tf-remote-state"
-    key    = "import-test" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
-    region = "us-east-1"
-    dynamodb_table = "82s-state-locking"
-  }
+  
 }
 
 provider "aws" {
